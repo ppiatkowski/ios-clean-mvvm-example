bc8689572cd8c8dfc910c2f7f71e87d6bf43155d
Paweł Piątkowski <pawel.piatkowski@mobica.com>
2018-12-07 12:37:55 +0100

    Created project

---
+++ CleanMVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CleanMVVM
+//
+//  Created by Paweł Piątkowski on 07/12/2018.
+//  Copyright © 2018 Paweł Piątkowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
